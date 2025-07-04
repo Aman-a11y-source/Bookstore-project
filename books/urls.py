903db7e80ded7f41712fa11f3e9946ee0a0d6396
Aman Brahma <atanuaman25@gmail.com>
2025-07-05 02:50:23 +0530

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("<str:author>",views.author, name="author.books"),
     path('register/', views.register, name='register'),
 
-    # New URLs for static pages
+   
     path('about-us/', views.about_us_view, name='about_us'),
     path('contact/', views.contact_view, name='contact'),
     path('privacy-policy/', views.privacy_policy_view, name='privacy_policy'),
